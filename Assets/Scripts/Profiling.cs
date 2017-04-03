34f2fe8779242b4bd4356d1326dca2eed902fdb5
DBeumans <buncho.gamingnetwork@gmail.com>
2017-04-03 10:56:18 +0200

    added project files

---
+++ Assets/Scripts/Profiling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Profiling : MonoBehaviour {
+	
+
+	void Start(){
+		GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		//timer = GetComponent<ProfilingTimer> ();
+
+		ProfilingTimer.StartProfilingTime ("Speed");
+
+		Instantiate (go);
+
+		ProfilingTimer.EndProfilingTime ("Speed");
+		ProfilingTimer.ShowProfilingTime ("Speed");
+/*
+
+		float start = Time.realtimeSinceStartup * 1000;
+		Instantiate (go);
+		float end = Time.realtimeSinceStartup * 1000;
+		Debug.Log( start - end);*/
+
+	}
+}
---
+++ Assets/Scripts/ProfilingTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfilingTimer : MonoBehaviour {
+
+	static float startTime;
+
+	private static readonly Dictionary<string,float> profilingTimeList = new Dictionary<string,float>();
+
+	public static void StartProfilingTime(string text) {
+		profilingTimeList[text] = Time.realtimeSinceStartup * 1000;
+		Debug.Log( text + " Time: " + "\n" +"MiliSeconds: "  + profilingTimeList[text]) ;
+	}
+
+	public static void EndProfilingTime(string text) {
+		if(!containsKey(text) )
+			return;
+		float end = Time.realtimeSinceStartup * 1000;
+		float diff = end - profilingTimeList[text];
+		Debug.Log( "Difference " +  text +": " + diff + "\n" + profilingTimeList[text]);
+	}
+
+	public static void removeTime(string time) {
+		profilingTimeList.Remove (time);
+	}
+
+	private static bool containsKey (string text) {
+		return profilingTimeList.ContainsKey (text); 
+	}
+
+	public static void ShowProfilingTime(string text) {
+		if(!containsKey(text) )
+			return;
+		Debug.Log (profilingTimeList[text]);
+	}
+}
